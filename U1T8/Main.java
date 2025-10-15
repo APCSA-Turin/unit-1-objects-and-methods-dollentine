14460627b4a627d319731fb54e75cf6e61c8d6b1
Rubya Anamika <rubyaa2@nycstudents.net>
2025-10-15 18:03:47 +0000

    Updated

---
+++ U1T8/Main.java
@@ -0,0 +1,11 @@
+package U1T8;
+
+import U1T8.GetLocation.GeoLocation;
+
+public class Main {
+   public static void main(String[] args) {
+      GeoLocation geo = new GeoLocation(14, 15);
+      geo.printCoords();
+
+   }
+}

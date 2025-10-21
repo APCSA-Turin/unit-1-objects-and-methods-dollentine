2f4881bb2a78779940ba3a86d1a1d159b8142363
Rubya Anamika <rubyaa2@nycstudents.net>
2025-10-21 18:03:21 +0000

    Updated

---
+++ U1T11/StringTest.java
@@ -0,0 +1,13 @@
+package U1T11;
+
+public class StringTest {
+    public static void main(String[] args) {
+
+        String str1 = "Hello how are you today?";
+        String str2 = "you";
+        String str3 = "You";
+        System.out.println (str1.indexOf(str2)); 
+        System.out.println (str1.indexOf(str3)); 
+
+    }
+}

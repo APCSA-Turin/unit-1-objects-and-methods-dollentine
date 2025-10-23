f8e3703894e6443e849a6d30b7cdbd79300cd049
Rubya Anamika <rubyaa2@nycstudents.net>
2025-10-23 18:02:12 +0000

    10/25/2025

---
+++ U1T11/StringInfo.java
@@ -0,0 +1,35 @@
+package U1T11;
+import java.util.Scanner;
+
+public class StringInfo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); 
+        System.out.print ("Enter first string: "); 
+        String word1 = scan.nextLine(); 
+        System.out.print ("Enter second string: "); 
+        String word2 = scan.nextLine(); 
+
+        if (word1.length() > word2.length()) {
+            System.out.println (word1 + " is longer"); 
+        } else {
+            System.out.println (word2 + " is longer"); 
+        }
+
+        int halfLen1 = (int) ((word1.length() / 2) + 0.5); 
+        System.out.println ("First half: " + word1.substring(0, halfLen1)); 
+        System.out.println ("Second half: " + word1.substring(halfLen1)); 
+
+        int halfLen2 = (int) ((word2.length() / 2) + 0.5); 
+        System.out.println ("First half: " + word2.substring(0, halfLen2)); 
+        System.out.println ("Second half: " + word2.substring(halfLen2)); 
+
+        if (word1.indexOf(word2) == -1) { 
+            System.out.println (word2 + " is not apart of " + word1); 
+        } else {
+            System.out.println (word2 + " is found in " + word1 + " at idex of " + word1.indexOf(word2));   
+        }
+
+        scan.close();        
+    }
+    
+}
--- U1T11/StringTest.java
+++ U1T11/StringTest.java
@@ -3,11 +3,11 @@ package U1T11;
 public class StringTest {
     public static void main(String[] args) {
 
-        String str1 = "Hello how are you today?";
-        String str2 = "you";
-        String str3 = "You";
-        System.out.println (str1.indexOf(str2)); 
-        System.out.println (str1.indexOf(str3)); 
+        String blah = "What's for dinner?";
+        String ugh = blah.substring(11, 17); 
+        System.out.println (ugh); 
+
+        
 
     }
 }
